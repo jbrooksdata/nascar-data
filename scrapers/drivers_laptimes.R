fcb96daf6b2a824d1bcf0eacb1bf12edcc973ad2
Jeremy Brooks <jeremydanielbrooks@yahoo.com>
2022-04-04 22:44:39 -0500

    created race_id for loop

--- scrapers/drivers_laptimes.R
+++ scrapers/drivers_laptimes.R
@@ -1,37 +1,52 @@
 library(jsonlite)
 library(dplyr)
 
-# feb 20 daytona 500 - id: 5146
-## some laps may be missing from source
-url_json <- "https://cf.nascar.com/cacher/2022/1/5146/lap-times.json"
-raw_json <- fromJSON(url_json)
+race_id <- "5146" # can enter list of race ids here
 
-drivers <- raw_json$laps
-laps <- drivers$Laps
+compile_stats <- data.frame()
 
-# end timeslist loop at max number of drivers per race
-finish <- 1:nrow(drivers)
-
-# create driver list; remove special characters (rookie designation, etc.)
-driverlist <- gsub("\\s*\\([^\\)]+\\)","",
-              gsub(" #","",
-              gsub("\\* ","",drivers$FullName)))
-
-# lap times dataframe loop
-timeslist <- data.frame()
-
-for(x in finish){
+for(i in race_id){
   
-  times <- (as.data.frame(drivers$Laps[x]))[2]
-  conv2cols <- as.data.frame(t(times)) # transpose to match driverlist
+  laps_json <- paste0("https://cf.nascar.com/cacher/2022/1/",i,"/lap-times.json")
+  raw_laps <- fromJSON(laps_json)
   
-  # bind_rows instead of rbind to ignore column lengths (DNF, laps down, etc.)
-  timeslist <- bind_rows(timeslist,conv2cols)
+  misc_json <- paste0("https://cf.nascar.com/live/feeds/series_1/",i,"/live_feed.json")
+  raw_misc <- fromJSON(misc_json)
+  
+  race_name <- raw_misc$run_name
+  race_id <- raw_misc$race_id
+  
+  drivers <- raw_laps$laps
+  laps <- drivers$Laps
+  
+  # max num of drivers per race
+  finish <- 1:nrow(drivers)
+  
+  # remove special characters via nested gsub
+  driverlist <- gsub("\\s*\\([^\\)]+\\)","",
+                     gsub(" #","",
+                          gsub("\\* ","",drivers$FullName)))
+  
+  # lap times dataframe loop
+  timeslist <- data.frame()
+  
+  for(x in finish){
+    
+    times <- (as.data.frame(drivers$Laps[x]))[2]
+    conv2cols <- as.data.frame(t(times))
+    
+    # bind_rows instead of rbind to ignore column lengths
+    add_race_info <- bind_cols(race_name,race_id,conv2cols)
+    timeslist <- bind_rows(timeslist,add_race_info)
+  }
+  
+  lapchart <- bind_cols(driverlist,timeslist)
+  rownames(lapchart) <- c(1:nrow(drivers))
+  names(lapchart)[1] <- "driver"
+  names(lapchart)[2] <- "race"
+  names(lapchart)[3] <- "race_id"
+  
+  compile_stats <- bind_rows(compile_stats,lapchart)
+  
+  
 }
-
-
-lapchart <- bind_cols(driverlist,timeslist)
-# rename rows 1:n=drivers in race
-rownames(lapchart) <- c(1:nrow(drivers))
-# first column will otherwise be a driver's name
-names(lapchart)[1] <- "driver"
